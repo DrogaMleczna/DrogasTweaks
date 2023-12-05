a21df4e22a454b82733445c9ebf2e326bc56082d
marce <marcelir2d2@gmail.com>
2023-12-05 21:50:30 +0100

    Pears now grow on willows

--- src/main/java/com/drogamleczna/drogastweaks/datagen/loot/ModBlockLootTables.java
+++ src/main/java/com/drogamleczna/drogastweaks/datagen/loot/ModBlockLootTables.java
@@ -1,22 +1,43 @@
 package com.drogamleczna.drogastweaks.datagen.loot;
 
 import com.drogamleczna.drogastweaks.block.ModBlocks;
+import com.drogamleczna.drogastweaks.item.ModItems;
+import net.minecraft.advancements.critereon.EnchantmentPredicate;
+import net.minecraft.advancements.critereon.ItemPredicate;
+import net.minecraft.advancements.critereon.MinMaxBounds;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.predicates.BonusLevelTableCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.MatchTool;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.Set;
 
 public class ModBlockLootTables extends BlockLootSubProvider {
+
+    protected static final LootItemCondition.Builder HAS_SILK_TOUCH = MatchTool.toolMatches(ItemPredicate.Builder.item().hasEnchantment(new EnchantmentPredicate(Enchantments.SILK_TOUCH, MinMaxBounds.Ints.atLeast(1))));
+    protected static final LootItemCondition.Builder HAS_NO_SILK_TOUCH = HAS_SILK_TOUCH.invert();
+    protected static final LootItemCondition.Builder HAS_SHEARS = MatchTool.toolMatches(ItemPredicate.Builder.item().of(Items.SHEARS));
+    private static final LootItemCondition.Builder HAS_SHEARS_OR_SILK_TOUCH = HAS_SHEARS.or(HAS_SILK_TOUCH);
+    private static final LootItemCondition.Builder HAS_NO_SHEARS_OR_SILK_TOUCH = HAS_SHEARS_OR_SILK_TOUCH.invert();
+
     public ModBlockLootTables() {
         super(Set.of(), FeatureFlags.REGISTRY.allFlags());
     }
 
     @Override
     protected void generate() {
+
         dropSelf(ModBlocks.FLINT_BLOCK.get());
 
         dropSelf(ModBlocks.WILLOW_PLANKS.get());
@@ -30,12 +51,19 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         dropSelf(ModBlocks.DEAD_TREE_SAPLING.get());
 
         add(ModBlocks.WILLOW_LEAVES.get(), block ->
-                createLeavesDrops(block, ModBlocks.WILLOW_SAPLING.get(), NORMAL_LEAVES_SAPLING_CHANCES));
+                createLeavesDropsWithItem(block, ModBlocks.WILLOW_SAPLING.get(), ModItems.PEAR.get(),NORMAL_LEAVES_SAPLING_CHANCES));
 
     }
 
     @Override
     protected Iterable<Block> getKnownBlocks() {
         return ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
+    }
+
+    protected LootTable.Builder createLeavesDropsWithItem(Block pOakLeavesBlock, Block pSaplingBlock, Item pItem , float... pChances) {
+        return this.createLeavesDrops(pOakLeavesBlock, pSaplingBlock, pChances).withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F))
+                .when(HAS_NO_SHEARS_OR_SILK_TOUCH).add(this.applyExplosionCondition(pOakLeavesBlock,
+                        LootItem.lootTableItem(pItem)).when(BonusLevelTableCondition.bonusLevelFlatChance(
+                                Enchantments.BLOCK_FORTUNE, 0.005F, 0.0055555557F, 0.00625F, 0.008333334F, 0.025F))));
     }
 }
